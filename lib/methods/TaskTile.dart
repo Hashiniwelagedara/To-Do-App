7f2c118d099a4613f3f62cd1f6b2ca080105b9f9
Hashini Welagedara <hashiniwel@gmail.com>
2023-10-09 22:02:38 +0530

    add task tile

--- lib/methods/TaskTile.dart
+++ lib/methods/TaskTile.dart
@@ -1,31 +1,57 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
- 
-  final bool? isChecked ;
+class TaskTile extends StatefulWidget {
+  bool? isChecked;
   final String? taskTitle;
 
   TaskTile({this.isChecked, this.taskTitle});
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  parantFunction(bool? newValue) {
+    setState(() {
+      widget.isChecked = newValue;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-       taskTitle!,
+        widget.taskTitle!,
         style: TextStyle(
-            decoration: isChecked! ? TextDecoration.lineThrough : null),
+            decoration: widget.isChecked! ? TextDecoration.lineThrough : null),
       ),
-      trailing:Checkbox (
-        onChanged: (value) {
-          
-        },
+      trailing: TaskCheckBox(
+        isChecked: widget.isChecked,
+        activeFunction: parantFunction,
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool? isChecked;
+  final Function(bool?)? activeFunction;
+
+  TaskCheckBox({
+    super.key,
+    required this.isChecked,
+    required this.activeFunction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      onChanged: activeFunction,
       activeColor: Colors.purple[700],
       value: isChecked,
 
       // onChanged: toggleCheckboxState,
-    ),
-          );
-    
+    );
   }
 }
 
